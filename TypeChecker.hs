4b75634d8c5f841c759e99dc3293127d15a48e48
Adam Michalik <adam@michalik.es>
2011-12-22 16:46:01 +0100

    typechecking typedefs

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -4,6 +4,8 @@ import AST
 
 import Control.Monad.State
 import Control.Monad.Error
+
+import Data.Maybe
 import qualified Data.Map as Map
                              
 data CType = CInt | CFloat | CBool | CVoid
@@ -13,29 +15,79 @@ data CType = CInt | CFloat | CBool | CVoid
            | CPointerType CType
            deriving Show
                     
+type CFunType = (CType, [CType])
+
 type TypeEnv = Map.Map String CType
 
 emptyTypeEnv = Map.fromList [("int", CInt), ("float", CFloat),
                              ("bool", CBool), ("void", CVoid)]
 
+type VarEnv = [Map.Map String CType]
+
+type FunEnv = Map.Map String CFunType
+
+data Env = Env {
+  typeEnv :: TypeEnv,
+  varEnv :: VarEnv,
+  funEnv :: FunEnv }
+
 data TypeError = TypeExistsError String CType
-                   | UnknownTypeError String
-                   | OtherTypeError String
-                   deriving Show
+               | UnknownTypeError String
+               | TypedefingCompositeType String CType
+               | TypedefedStructIsNotStruct String CType
+               | OtherTypeError String
+               deriving Show
                             
 instance Error TypeError where
   noMsg = OtherTypeError "type error occured"
   strMsg = OtherTypeError
   
 newtype TypeChecker a = Checker {
-  runChecker :: ErrorT TypeError (State TypeEnv) a
+  runChecker :: ErrorT TypeError (State Env) a
   } deriving (Monad, MonadError TypeError)
 
 liftChecker m = Checker (lift m)
   
+getEnv = liftChecker get
+
+getType name = getEnv >>= return . typeEnv >>= return . Map.lookup name
+
+putEnv e = liftChecker $ put e
+
+putType name t = do
+  env <- getEnv
+  let tEnv = typeEnv env
+  putEnv $ env {typeEnv = Map.insert name t tEnv}
+
+declToType (CPrimitiveTypeDeclaration "int") = return CInt
+declToType (CPrimitiveTypeDeclaration "float") = return CFloat
+declToType (CPrimitiveTypeDeclaration "bool") = return CBool
+declToType (CPrimitiveTypeDeclaration "void") = return CVoid
+
+declToType (CTypedefTypeDeclaration name) = do
+  t <- getType name
+  case t of
+    Just t -> case t of
+      CTypedefType _ t -> return t
+      t -> throwError (TypedefingCompositeType name t)
+    Nothing -> throwError (UnknownTypeError name)
+    
+declToType (CStructDeclaration name) = do
+  t <- getType name
+  case t of
+    Just t -> case t of
+      s@(CStructType _ _) -> return s
+      t -> throwError (TypedefedStructIsNotStruct name t)
+    Nothing -> throwError (UnknownTypeError name)
+
+typeCheck (CTypedefDefinition decl name) = do
+  t <- getType name
+  case t of
+    Just t -> throwError (TypeExistsError name t)
+    Nothing -> declToType decl >>= putType name
+  
 typeChecker :: CTranslationUnit -> TypeChecker ()
-typeChecker unit = do
-  return ()
+typeChecker = mapM_ typeCheck
 
 runParser p s = case runState (runErrorT (runChecker p)) s of
   (Left err, _) -> Left err
